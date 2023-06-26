253ceff8c35f094ae62a764661ba53259d816de4
theodcod <attahassani@hotmail.com>
2023-06-26 16:25:09 -0700

    tex upd

--- auto/notes.el
+++ auto/notes.el
@@ -2,20 +2,20 @@
  "notes"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("article" "12pt")))
+                     '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
-    "art12"
+    "art11"
     "inputenc"
     "fontenc"
     "graphicx"
@@ -30,17 +30,22 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:orgc7221f7"
-    "sec:orgf680af5"
-    "sec:orgd8cdd42"
-    "sec:org2cccb56"
-    "sec:orgf999f3a"
-    "sec:org337369e"
-    "sec:orge2f39d0"
-    "sec:orgff63a2a"
-    "sec:orgcb42720"
-    "sec:org4dd9881"
-    "sec:org1433ded"
-    "sec:orgd2bf8ea"))
+    "sec:org6900061"
+    "sec:orgbe238a5"
+    "sec:orge55d623"
+    "sec:orgc2b4ee8"
+    "sec:org051cc14"
+    "sec:org3c3eba9"
+    "sec:orgc54031c"
+    "sec:orgc26e6da"
+    "sec:org3b8a64c"
+    "sec:orgde8bdfb"
+    "sec:org217d6ab"
+    "sec:org40cab40"
+    "sec:org0354df6"
+    "sec:orgb260749"
+    "sec:org8653ee4"
+    "sec:org7033066"
+    "sec:orgb444a99"))
  :latex)
 
