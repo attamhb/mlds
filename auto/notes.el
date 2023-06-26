9e2b258971ba5855156c15618c00fa4640424f87
theodcod <attahassani@hotmail.com>
2023-06-26 16:19:25 -0700

    updated dnn part

---
+++ auto/notes.el
@@ -0,0 +1,46 @@
+(TeX-add-style-hook
+ "notes"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgc7221f7"
+    "sec:orgf680af5"
+    "sec:orgd8cdd42"
+    "sec:org2cccb56"
+    "sec:orgf999f3a"
+    "sec:org337369e"
+    "sec:orge2f39d0"
+    "sec:orgff63a2a"
+    "sec:orgcb42720"
+    "sec:org4dd9881"
+    "sec:org1433ded"
+    "sec:orgd2bf8ea"))
+ :latex)
+
